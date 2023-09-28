c3742af7bb2005c10848ce99358ad3ffd00e80bd
suraphop <mic@admin.com>
2023-09-28 16:23:14 +0700

    fix bug

--- jenkins/build/start.sh
+++ jenkins/build/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-streamlit run main_config.py --server.port=8501 --server.address=0.0.0.0 && cron -f
+cron -f &
+streamlit run main_config.py --server.port=8501 --server.address=0.0.0.0
